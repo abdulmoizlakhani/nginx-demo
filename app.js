97b21e211702fc8effadc93bdfc61d60beed7d31
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2022-06-07 21:20:58 +0500

    - created pages demp

--- app.js
+++ app.js
@@ -6,16 +6,17 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/url/meta/:lang?", (req, res) => {
-  const lang = req.params.lang;
-  if (lang) {
-    if (lang === "en") {
-      res.sendFile(`./page.html`, { root: __dirname + "/public" });
-    } else {
-      res.sendFile(`./${lang}/page.html`, { root: __dirname + "/public" });
-    }
+app.use("/url/meta/:page", (req, res) => {
+  const lang = req.query.lang;
+  const page = req.params.page ? req.params.page.toLowerCase() : undefined;
+  if (!lang && page) {
+    res.sendFile(`./en/${page}.html`, { root: __dirname + "/public" });
+  } else if (lang && !page) {
+    res.sendFile(`./${lang}/home.html`, { root: __dirname + "/public" });
+  } else if (!lang && !page) {
+    res.sendFile(`./en/home.html`, { root: __dirname + "/public" });
   } else {
-    res.redirect("/url/meta/en");
+    res.sendFile(`./${lang}/${page}.html`, { root: __dirname + "/public" });
   }
 });
 
